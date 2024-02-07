59d046847a4daf0e74b84497f15b5ec6d30f425a
Tawanna <thol_16@hotmail.com>
2024-02-06 21:31:41 -0300

    insert review implemented

--- src/main/java/com/devsuperior/movieflix/dto/ReviewDTO.java
+++ src/main/java/com/devsuperior/movieflix/dto/ReviewDTO.java
@@ -1,5 +1,8 @@
 package com.devsuperior.movieflix.dto;
 
+import com.devsuperior.movieflix.entities.Movie;
+import com.devsuperior.movieflix.entities.Review;
+import com.devsuperior.movieflix.entities.User;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 
@@ -11,9 +14,21 @@ public class ReviewDTO {
     private String text;
 
     @NotNull(message = "Campo requerido")
-    private Long movieId;    
-    
-    private Long userId;
+    private Long movieId;
+
+	public ReviewDTO() {
+	}
+
+	public ReviewDTO(Review entity, Movie movie, User user) {
+		this.id = entity.getId();
+		this.text = entity.getText();
+		this.movieId = movie.getId();
+		this.userId = user.getId();
+		this.userName = user.getName();
+		this.userEmail = user.getEmail();
+	}
+
+	private Long userId;
     private String userName;
     private String userEmail;
     
---
+++ src/main/java/com/devsuperior/movieflix/services/ReviewService.java
@@ -0,0 +1,40 @@
+package com.devsuperior.movieflix.services;
+
+import com.devsuperior.movieflix.dto.ReviewDTO;
+import com.devsuperior.movieflix.entities.Movie;
+import com.devsuperior.movieflix.entities.Review;
+import com.devsuperior.movieflix.entities.User;
+import com.devsuperior.movieflix.repositories.MovieRepository;
+import com.devsuperior.movieflix.repositories.ReviewRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+public class ReviewService {
+    @Autowired
+    private ReviewRepository repository;
+
+    @Autowired
+    private MovieRepository movieRepository;
+
+    @Autowired
+    private AuthService authService;
+
+    @Transactional
+    public ReviewDTO insert(ReviewDTO reviewDTO){
+        Review review = new Review();
+        review.setText(reviewDTO.getText());
+
+        Movie movie = movieRepository.getReferenceById(reviewDTO.getMovieId());
+        review.setMovie(movie);
+
+        User user = authService.authenticated();
+        review.setUser(user);
+
+        review = repository.save(review);
+
+        return new ReviewDTO(review, review.getMovie(), review.getUser());
+
+    }
+}
